e1f439076d6f19e6bc3e50cfdd449ebde8967dad
Royce Schultz <rosc1351@colorado.edu>
2020-08-19 18:50:20 -0600

    modifies tests to work with new functional approach

--- test/test_Game.py
+++ test/test_Game.py
@@ -1,58 +1,93 @@
 def test_Game_is_importable():
-    from Scrabble.src.Game import Game
+    from Scrabble.src import Game
 
 def test_init():
-    from Scrabble.src.Game import Game
-    game = Game()
+    from Scrabble.src import Game
+    game = Game.new_game()
 
 def test_current_player():
-    from Scrabble.src.Game import Game
-    game = Game()
-    assert game.current_player().name == 'P1'
+    from Scrabble.src import Game
+    game = Game.new_game()
+    assert Game.get_current_player(game)['name'] == 'P1'
 
 
 def test_make_move():
-    from Scrabble.src.Game import Game
-    game = Game()
-    game.current_player().rack = {'h':3,'i':4}
+    import Scrabble.src.Game as Game
+    game = Game.new_game()
+    Game.get_current_player(game)['rack'] = {'h':3,'i':4}
     move = {
         (7,7): 'h',
         (7,8): 'i',
     }
-    game.play('P1', move)
-    assert game.board[(7,7)] == 'h'
-    assert game.board[(7,8)] == 'i'
-    assert game.current_player().name == 'P2'
+    Game.play(game,'P1', move)
+    assert game['played_tiles'][(7,7)] == 'h'
+    assert game['played_tiles'][(7,8)] == 'i'
+    assert Game.get_current_player(game)['name'] == 'P2'
 
 def test_make_2_moves():
-    from Scrabble.src.Game import Game
-    game = Game()
-    game.current_player().rack = {'h':3,'i':4}
+    import Scrabble.src.Game as Game
+    game = Game.new_game()
+    Game.get_current_player(game)['rack'] = {'h':3,'i':4}
     move = {
         (7,7): 'h',
         (7,8): 'i',
     }
-    game.play('P1', move)
-    assert game.board[(7,7)] == 'h'
-    assert game.board[(7,8)] == 'i'
-    assert game.current_player().name == 'P2'
-    game.current_player().rack = {'p':3,'i':4}
+    Game.play(game, 'P1', move)
+    assert game['played_tiles'][(7,7)] == 'h'
+    assert game['played_tiles'][(7,8)] == 'i'
+    assert Game.get_current_player(game)['name'] == 'P2'
+    Game.get_current_player(game)['rack'] = {'p':3,'i':4}
     move = {
         (8,7): 'i',
     }
-    game.play('P2', move)
-    assert game.board[(7,7)] == 'h'
-    assert game.board[(8,7)] == 'i'
-    assert game.current_player().name == 'P1'
+    Game.play(game,'P2', move)
+    assert game['played_tiles'][(7,7)] == 'h'
+    assert game['played_tiles'][(8,7)] == 'i'
+    assert Game.get_current_player(game)['name'] == 'P1'
+
+def test_invalid_first_move():
+    from Scrabble.src import Game
+    game = Game.new_game()
+    Game.get_current_player(game)['rack'] = {'h':3,'i':4}
+    move = {
+        (7,8): 'h',
+        (7,9): 'i',
+    }
+    try:
+        Game.play(game,'P1', move)
+        assert False
+    except:
+        assert Game.get_current_player(game)['name'] == 'P1'
+
+def test_invalid_second_move():
+    from Scrabble.src import Game
+    game = Game.new_game()
+    Game.get_current_player(game)['rack'] = {'h':3,'i':4}
+    move = {
+        (7,7): 'h',
+        (7,8): 'i',
+    }
+    Game.play(game,'P1', move)
+    assert game['played_tiles'][(7,7)] == 'h'
+    assert game['played_tiles'][(7,8)] == 'i'
+    assert Game.get_current_player(game)['name'] == 'P2'
+    Game.get_current_player(game)['rack'] = {'p':3,'i':4}
+    move = {
+        (8,7): 'i',
+    }
+    Game.play(game,'P2', move)
+    assert game['played_tiles'][(7,7)] == 'h'
+    assert game['played_tiles'][(8,7)] == 'i'
+    assert Game.get_current_player(game)['name'] == 'P1'
 
 def test_3_player_turn_rotation():
-    from Scrabble.src.Game import Game
-    game = Game(players=['1','2','3'])
-    assert game.current_player().name == '1'
-    game.end_turn()
-    assert game.current_player().name == '2'
-    game.end_turn()
-    assert game.current_player().name == '3'
-    game.end_turn()
-    assert game.current_player().name == '1'
-    game.end_turn()
+    from Scrabble.src import Game
+    game = Game.new_game(players=['1','2','3'])
+    assert Game.get_current_player(game)['name'] == '1'
+    Game.end_turn(game)
+    assert Game.get_current_player(game)['name'] == '2'
+    Game.end_turn(game)
+    assert Game.get_current_player(game)['name'] == '3'
+    Game.end_turn(game)
+    assert Game.get_current_player(game)['name'] == '1'
+    Game.end_turn(game)
